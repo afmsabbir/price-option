ef9e0b2896fbeb374683fc7f4044333d05ef9df7
afmsabbir <khanafmsabbir@gmail.com>
2024-03-21 17:57:19 +0600

    Explore React Awesome Components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip,} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { Audio } from "react-loader-spinner";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios
       .get("https://openapi.programming-hero.com/api/phones?search=iphone")
@@ -17,16 +19,30 @@ const Phones = () => {
           return obj;
         });
         setPhones(phonesWithFakeData);
+        setLoading(false);
       });
   }, []);
   return (
     <div>
+      {loading && (
+        <div>
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+      )}
       <h1>phones: {phones.length}</h1>
       <BarChart width={1200} height={400} data={phones}>
-          <Bar dataKey="price" fill="#8884d8" />
-          <XAxis dataKey='name'></XAxis>
-          <YAxis dataKey='price'></YAxis>
-          <Tooltip></Tooltip>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis dataKey="price"></YAxis>
+        <Tooltip></Tooltip>
       </BarChart>
     </div>
   );
